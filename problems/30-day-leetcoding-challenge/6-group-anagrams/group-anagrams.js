67e18baea58bf1a808af61b0d7676f45903af7c0
Baks Amrenov <rlyawesome1@gmail.com>
2020-04-12 14:58:41 +0600

    feat: add group anagrams solution

---
+++ problems/30-day-leetcoding-challenge/6-group-anagrams/group-anagrams.js
@@ -0,0 +1,18 @@
+// Time Complexity: O(NK), where N is the length of strs, and K is the maximum length of a string in strs.
+// Space Complexity: O(NK)
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+const groupAnagrams = (strs) => {
+    const map = new Map();
+
+    for (let s of strs) {
+        let s2 = s;
+        s2 = s2.split('').sort().join();
+        if (!map.get(s2)) map.set(s2, []);
+        map.get(s2).push(s);
+    }
+
+    return Array.from(map.values());
+};
