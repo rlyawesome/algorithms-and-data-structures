0f4034a1b5eab2c5e7b813e0cb7bd62a70b520f7
Baks Amrenov <rlyawesome1@gmail.com>
2020-04-20 02:28:26 +0600

    feat: add product of array except self

---
+++ problems/30-day-leetcoding-challenge/15-product-of-array-except-self/product-of-array-except-self.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const productExceptSelf = (nums) => {
+    let output = [];
+    output[0] = 1;
+    for (let i = 1; i < nums.length; i++) {
+        output[i] = output[i - 1] * nums[i - 1];
+    }
+    let r = 1;
+    for (let i = output.length - 1; i >= 0; i--) {
+        output[i] = output[i] * r;
+        r *= nums[i];
+    }
+    return output;
+};
