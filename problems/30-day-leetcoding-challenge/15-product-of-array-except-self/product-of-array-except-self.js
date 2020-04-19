e8f32571599046da3ad01ffccc186b4087a923c4
Baks Amrenov <rlyawesome1@gmail.com>
2020-04-20 02:30:50 +0600

    fix: change let to const

--- problems/30-day-leetcoding-challenge/15-product-of-array-except-self/product-of-array-except-self.js
+++ problems/30-day-leetcoding-challenge/15-product-of-array-except-self/product-of-array-except-self.js
@@ -3,7 +3,7 @@
  * @return {number[]}
  */
 const productExceptSelf = (nums) => {
-    let output = [];
+    const output = [];
     output[0] = 1;
     for (let i = 1; i < nums.length; i++) {
         output[i] = output[i - 1] * nums[i - 1];
