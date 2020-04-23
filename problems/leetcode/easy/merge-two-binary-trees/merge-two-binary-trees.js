aa6b8fa2289d865709ba7f8d6b2ae2964e611b0a
Baks Amrenov <rlyawesome1@gmail.com>
2020-04-23 17:27:42 +0600

    feat: merge two binary trees

---
+++ problems/leetcode/easy/merge-two-binary-trees/merge-two-binary-trees.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} t1
+ * @param {TreeNode} t2
+ * @return {TreeNode}
+ */
+const mergeTrees = (t1, t2) => {
+    if (t1 == null || t2 == null) return t1 || t2;
+
+    const root = new TreeNode(t1.val + t2.val);
+    root.left = mergeTrees(t1.left, t2.left);
+    root.right = mergeTrees(t1.right, t2.right);
+
+    return root;
+};
