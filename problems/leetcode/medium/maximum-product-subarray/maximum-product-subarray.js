2ce9adb6822e5aa61a0fe8e8d11aff54d260598a
Baks Amrenov <rlyawesome1@gmail.com>
2020-05-27 14:37:00 +0600

    feat(medium): add another approach to max product subarray

--- problems/leetcode/medium/maximum-product-subarray/maximum-product-subarray.js
+++ problems/leetcode/medium/maximum-product-subarray/maximum-product-subarray.js
@@ -1,3 +1,4 @@
+// Approach 1
 /**
  * @param {number[]} nums
  * @return {number}
@@ -12,6 +13,27 @@ const maxProduct = (nums) => {
         l = (l ? l : 1) * nums[i];
         r = (r ? r : 1) * nums[N - 1 - i];
         ans = Math.max(ans, l, r);
+    }
+
+    return ans;
+};
+
+// Approach 2
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const maxProduct = (nums) => {
+    const N = nums.length;
+    let ans = nums[0];
+    let min = nums[0];
+    let max = nums[0];
+
+    for (let i = 1; i < N; i++) {
+        if (nums[i] < 0) [max, min] = [min, max];
+        max = Math.max(nums[i], nums[i] * max);
+        min = Math.min(nums[i], nums[i] * min);
+        ans = Math.max(ans, max);
     }
 
     return ans;
