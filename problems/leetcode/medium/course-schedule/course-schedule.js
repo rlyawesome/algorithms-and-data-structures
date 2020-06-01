c4b3970ae386a1bf9073826e96a4b3105e79ce28
Baks Amrenov <rlyawesome1@gmail.com>
2020-06-01 14:10:47 +0600

    refactor: make course schedule code more concise

--- problems/leetcode/medium/course-schedule/course-schedule.js
+++ problems/leetcode/medium/course-schedule/course-schedule.js
@@ -4,33 +4,31 @@
  * @return {boolean}
  */
 const canFinish = (numCourses, prerequisites) => {
-    const inDegree = new Array(numCourses).fill(0);
+  const degree = new Array(numCourses).fill(0);
 
-    for (let i = 0; i < prerequisites.length; i++) {
-        inDegree[prerequisites[i][0]]++;
+  for (const e of prerequisites) {
+    degree[e[0]]++;
+  }
+
+  const stack = [];
+
+  for (let i = 0; i < degree.length; i++) {
+    if (degree[i] == 0) stack.push(i);
+  }
+
+  let count = 0;
+
+  while (stack.length) {
+    const curr = stack.pop();
+    count++;
+
+    for (const e of prerequisites) {
+      if (e[1] == curr) {
+        degree[e[0]]--;
+        if (degree[e[0]] == 0) stack.push(e[0]);
+      }
     }
+  }
 
-    const stack = [];
-
-    for (let i = 0; i < inDegree.length; i++) {
-        if (inDegree[i] == 0) stack.push(i);
-    }
-
-    let count = 0;
-
-    while (stack.length) {
-        const curr = stack.pop();
-        count++;
-
-        for (let i = 0; i < prerequisites.length; i++) {
-            if (prerequisites[i][1] == curr) {
-                inDegree[prerequisites[i][0]]--;
-                if (inDegree[prerequisites[i][0]] == 0) {
-                    stack.push(prerequisites[i][0]);
-                }
-            }
-        }
-    }
-
-    return count == numCourses;
+  return count == numCourses;
 };
