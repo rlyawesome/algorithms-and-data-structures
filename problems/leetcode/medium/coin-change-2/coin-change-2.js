395f2280e75fab617694eb75a030b98f26ee8dbb
Baks Amrenov <rlyawesome1@gmail.com>
2020-06-08 00:40:45 +0600

    feat(medium): coin change 2 - improved space complexity

--- problems/leetcode/medium/coin-change-2/coin-change-2.js
+++ problems/leetcode/medium/coin-change-2/coin-change-2.js
@@ -1,3 +1,4 @@
+// Approach 1: Knapsack problem
 /**
  * @param {number} amount
  * @param {number[]} coins
@@ -24,4 +25,23 @@ const change = (amount, coins) => {
   }
 
   return dp[N][amount];
+};
+
+// Approach 2: improved space complexity O(N)
+/**
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+const change = (amount, coins) => {
+  const dp = new Array(amount + 1).fill(0);
+  dp[0] = 1;
+
+  for (const coin of coins) {
+    for (let i = coin; i <= amount; i++) {
+      dp[i] += dp[i - coin];
+    }
+  }
+
+  return dp[amount];
 };
